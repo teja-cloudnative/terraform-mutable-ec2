d4075dd32e7e094fd95a956337a22e4bd7f2a059
tejap <btejapramod@gmail.com>
2022-02-24 16:14:48 +0530

    New Changes

--- ec2.tf
+++ ec2.tf
@@ -3,3 +3,22 @@ resource "aws_instance" "od-instance" {
   instance_type = var.OD_INSTANCE_TYPE
   ami = data.aws_ami.ami.id
 }
+
+resource "aws_spot_instance_request" "spot-instance" {
+  count                  = var.SPOT_INSTANCE_COUNT
+  ami                    = data.aws_ami.ami.id
+  instance_type          = var.SPOT_INSTANCE_TYPE
+  wait_for_fulfillment   = true
+}
+
+#resource "aws_ec2_tag" "ec2-name-tag" {
+#  resource_id = aws_spot_instance_request.spot-instance.spot_instance_id
+#  key         = "Name"
+#  value       = var.COMPONENT["name"]
+#}
+#
+#resource "aws_ec2_tag" "ec2-monitor-tag" {
+#  resource_id = aws_spot_instance_request.od-.spot_instance_id
+#  key         = "Monitor"
+#  value       = var.COMPONENT["monitor"]
+#}
