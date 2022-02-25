eb88706abf8e0e339db5d8844d5441019038ce75
tejap <btejapramod@gmail.com>
2022-02-25 18:11:03 +0530

    New Changes

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   ALL_OD_INSTANCE_IDS    = concat(aws_instance.od-instance.*.id, aws_spot_instance_request.spot-instance.*.spot_instance_id)
-  ALL_SPOT_INSTANCE_IDS  = aws_spot_instance_request.spot-instance.*.id
+  ALL_SPOT_INSTANCE_IDS  = aws_spot_instance_request.spot-instance.id
   ALL_TAG_IDS            = concat(local.ALL_OD_INSTANCE_IDS, local.ALL_SPOT_INSTANCE_IDS, local.ALL_VOLUME_IDS)
   TAG_NAME               = "${var.COMPONENT}-${var.ENV}"
   SPOT_VOLUME_ID         = [for m in flatten(aws_spot_instance_request.spot-instance.*.root_block_device) : m.volume_id]
