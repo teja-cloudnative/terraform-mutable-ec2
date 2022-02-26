8749feb1ec61e9440ced744af432212b01018c0b
tejap <btejapramod@gmail.com>
2022-02-26 16:13:01 +0530

    New Changes

--- tags.tf
+++ tags.tf
@@ -1,28 +1,28 @@
-#module "tags" {
-#  count       = length(local.ALL_TAGS)
-#  source      = "git::github.com/teja-cloudnative/terraform-tags"
-#  TAG_NAME    = lookup(element(local.ALL_TAGS, count.index), "name")
-#  TAG_VALUE   = lookup(element(local.ALL_TAGS, count.index), "value")
-#  RESOURCE_ID = local.ALL_TAG_IDS
-#}
-#
-#locals {
-#  ALL_TAGS = [
-#    {
-#      name   = "name"
-#      value  = "${var.COMPONENT}-${var.ENV}"
-#    },
-#    {
-#      name   = "name"
-#      value  = var.ENV
-#    },
-#    {
-#      name   = "component"
-#      value  = var.COMPONENT
-#    },
-#    {
-#      name   = "project_name"
-#      value  = "roboshop"
-#    }
-#  ]
-#}
+module "tags" {
+  count       = length(local.ALL_TAGS)
+  source      = "git::github.com/teja-cloudnative/terraform-tags"
+  TAG_NAME    = lookup(element(local.ALL_TAGS, count.index), "name")
+  TAG_VALUE   = lookup(element(local.ALL_TAGS, count.index), "value")
+  RESOURCE_ID = local.ALL_TAG_IDS
+}
+
+locals {
+  ALL_TAGS = [
+    {
+      name   = "name"
+      value  = "${var.COMPONENT}-${var.ENV}"
+    },
+    {
+      name   = "name"
+      value  = var.ENV
+    },
+    {
+      name   = "component"
+      value  = var.COMPONENT
+    },
+    {
+      name   = "project_name"
+      value  = "roboshop"
+    }
+  ]
+}
