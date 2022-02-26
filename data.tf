4ff34affa02fe48c9cbda84255671acee52f6694
tejap <btejapramod@gmail.com>
2022-02-26 16:25:40 +0530

    New Changes

--- data.tf
+++ data.tf
@@ -7,7 +7,7 @@ data "aws_ami" "ami" {
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "terraform-b62"
+    bucket = "terraform-teja"
     key    = "mutable/vpc/${var.ENV}/terraform.tfstate"
     region = "us-east-1"
   }
