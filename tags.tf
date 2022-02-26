eca1460eefa6f46edebef5fd7cb2f494108861fd
tejap <btejapramod@gmail.com>
2022-02-26 16:10:23 +0530

    New Changes

--- tags.tf
+++ tags.tf
@@ -1,36 +1,28 @@
-#
-#resource "aws_ec2_tag" "ec2-name-tag" {
-#  count       = length(local.ALL_TAG_IDS)
-#  resource_id = local.ALL_TAG_IDS[count.index]
-#  key         = "Name"
-#  value       = local.TAG_NAME
+#module "tags" {
+#  count       = length(local.ALL_TAGS)
+#  source      = "git::github.com/teja-cloudnative/terraform-tags"
+#  TAG_NAME    = lookup(element(local.ALL_TAGS, count.index), "name")
+#  TAG_VALUE   = lookup(element(local.ALL_TAGS, count.index), "value")
+#  RESOURCE_ID = local.ALL_TAG_IDS
 #}
-
-module "tags" {
-  count       = length(local.ALL_TAGS)
-  source      = "git::github.com/teja-cloudnative/terraform-tags"
-  TAG_NAME    = lookup(element(local.ALL_TAGS, count.index), "name")
-  TAG_VALUE   = lookup(element(local.ALL_TAGS, count.index), "value")
-  RESOURCE_ID = local.ALL_TAG_IDS
-}
-
-locals {
-  ALL_TAGS = [
-    {
-      name   = "name"
-      value  = "${var.COMPONENT}-${var.ENV}"
-    },
-    {
-      name   = "name"
-      value  = var.ENV
-    },
-    {
-      name   = "component"
-      value  = var.COMPONENT
-    },
-    {
-      name   = "project_name"
-      value  = "roboshop"
-    }
-  ]
-}
+#
+#locals {
+#  ALL_TAGS = [
+#    {
+#      name   = "name"
+#      value  = "${var.COMPONENT}-${var.ENV}"
+#    },
+#    {
+#      name   = "name"
+#      value  = var.ENV
+#    },
+#    {
+#      name   = "component"
+#      value  = var.COMPONENT
+#    },
+#    {
+#      name   = "project_name"
+#      value  = "roboshop"
+#    }
+#  ]
+#}
