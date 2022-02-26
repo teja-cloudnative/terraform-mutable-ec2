68a50cb9dbd9c2c55b29a8e7fd1a38f45c97bc91
tejap <btejapramod@gmail.com>
2022-02-26 12:16:00 +0530

    New Changes

--- ec2.tf
+++ ec2.tf
@@ -16,8 +16,7 @@ resource "aws_spot_instance_request" "spot-instance" {
 
 resource "aws_ec2_tag" "ec2-name-tag" {
   count       = length(local.ALL_TAG_IDS)
-  resource_id = local.ALL_TAG_IDS
-  #[count.index]
+  resource_id = local.ALL_TAG_IDS[count.index]
   key         = "Name"
   value       = local.TAG_NAME
 }
