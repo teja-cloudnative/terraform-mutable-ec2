a7dea3b1114b0e278ce3e69be5b71ce22faa1535
tejap <btejapramod@gmail.com>
2022-02-28 17:40:07 +0530

    New Changes

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "od-instance" {
   instance_type = var.OD_INSTANCE_TYPE
   ami           = data.aws_ami.ami.id
   subnet_id     = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_ID, count.index)
+  vpc_security_group_ids = [aws_security_group.allow.id]
 }
 
 resource "aws_spot_instance_request" "spot-instance" {
@@ -11,6 +12,7 @@ resource "aws_spot_instance_request" "spot-instance" {
   instance_type          = var.SPOT_INSTANCE_TYPE
   wait_for_fulfillment   = true
   subnet_id     = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_ID, count.index)
+  vpc_security_group_ids = [aws_security_group.allow.id]
   //  root_block_device {
   //    volume_size = 10
   //  }
--- sg.tf
+++ sg.tf
@@ -1,15 +1,14 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      =
+resource "aws_security_group" "allow" {
+  name        = "${var.COMPONENT}-${var.ENV}-sg"
+  description = "${var.COMPONENT}-${var.ENV}-sg"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
   }
 
   egress {
@@ -17,7 +16,6 @@ resource "aws_security_group" "allow_tls" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
