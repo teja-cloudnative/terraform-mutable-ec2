db12038be9cbc954ae4ae3dd71530d89d66d44f6
tejap <btejapramod@gmail.com>
2022-02-28 17:22:54 +0530

    New Changes

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "od-instance" {
   count         = var.OD_INSTANCE_COUNT
   instance_type = var.OD_INSTANCE_TYPE
   ami           = data.aws_ami.ami.id
-  subnet_id     = element(data.terraform_remote_state.vpc.outputs.SUBNETS_ID, count.index)
+  subnet_id     = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_ID, count.index)
 }
 
 resource "aws_spot_instance_request" "spot-instance" {
@@ -10,7 +10,7 @@ resource "aws_spot_instance_request" "spot-instance" {
   ami                    = data.aws_ami.ami.id
   instance_type          = var.SPOT_INSTANCE_TYPE
   wait_for_fulfillment   = true
-  subnet_id     = element(data.terraform_remote_state.vpc.outputs.SUBNETS_ID, count.index)
+  subnet_id     = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_ID, count.index)
   //  root_block_device {
   //    volume_size = 10
   //  }
