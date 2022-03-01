2bdc9f4631879d66acbb85c6ceafb2174c9092c9
tejap <btejapramod@gmail.com>
2022-03-01 12:10:38 +0530

    New Changes

--- alb.tf
+++ alb.tf
@@ -1,12 +1,13 @@
-#resource "aws_lb_target_group" "tg" {
-#  name = "${var.COMPONENT}-${var.ENV}"
-#  port = var.PORT
-#  protocol = "HTTP"
-#  vpc_id = data.terraform_remote_state.vpc.outputs.VPC_ID
-#}
-#
-#resource "aws_lb_target_group_attachment" "test" {
-#  target_group_arn = aws_lb_target_group.test.arn
-#  target_id        = aws_instance.test.id
-#  port             = 80
-#}
+resource "aws_lb_target_group" "tg" {
+  name = "${var.COMPONENT}-${var.ENV}"
+  port = var.PORT
+  protocol = "HTTP"
+  vpc_id = data.terraform_remote_state.vpc.outputs.VPC_ID
+}
+
+resource "aws_lb_target_group_attachment" "tg-attach" {
+  count            = length(local.ALL_INSTANCE_IDS)
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = element(local.ALL_INSTANCE_IDS, count.index )
+  port             = var.PORT
+}
--- locals.tf
+++ locals.tf
@@ -1,7 +1,8 @@
 locals {
-  ALL_OD_INSTANCE_IDS   = concat(aws_instance.od-instance.*.id, aws_spot_instance_request.spot-instance.*.spot_instance_id)
+  ALL_INSTANCE_IDS   = concat(aws_instance.od-instance.*.id, aws_spot_instance_request.spot-instance.*.spot_instance_id)
   ALL_SPOT_INSTANCE_IDS = aws_spot_instance_request.spot-instance.*.id
-  ALL_TAG_IDS           = concat(local.ALL_OD_INSTANCE_IDS, local.ALL_SPOT_INSTANCE_IDS, local.ALL_VOLUME_IDS)
+  ALL_OD_INSTANCE_IDS   = aws_instance.od-instance.*.id
+  ALL_TAG_IDS           = concat(local.ALL_INSTANCE_IDS, local.ALL_SPOT_INSTANCE_IDS, local.ALL_VOLUME_IDS)
   TAG_NAME              = "${var.COMPONENT}-${var.ENV}"
   SPOT_VOLUME_ID        = [for m in flatten(aws_spot_instance_request.spot-instance.*.root_block_device) : m.volume_id]
   OD_VOLUME_ID          = [for m in flatten(aws_instance.od-instance.*.root_block_device) : m.volume_id]
