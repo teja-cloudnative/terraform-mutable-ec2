52ab086707b3075068383f1adbb05afa719641c7
tejap <btejapramod@gmail.com>
2022-03-01 12:05:56 +0530

    New Changes

--- alb.tf
+++ alb.tf
@@ -1,6 +1,12 @@
-resource "aws_lb_target_group" "frontend" {
-  name = "${var.COMPONENT}-${var.ENV}"
-  port = var.PORT
-  protocol = "HTTP"
-  vpc_id = data.terraform_remote_state.vpc.outputs.VPC_ID
-}
+#resource "aws_lb_target_group" "tg" {
+#  name = "${var.COMPONENT}-${var.ENV}"
+#  port = var.PORT
+#  protocol = "HTTP"
+#  vpc_id = data.terraform_remote_state.vpc.outputs.VPC_ID
+#}
+#
+#resource "aws_lb_target_group_attachment" "test" {
+#  target_group_arn = aws_lb_target_group.test.arn
+#  target_id        = aws_instance.test.id
+#  port             = 80
+#}
