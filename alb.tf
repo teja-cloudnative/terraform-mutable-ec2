691c2fe2487bd9b1b5563b4da37581bf296a1121
tejap <btejapramod@gmail.com>
2022-03-01 13:25:36 +0530

    New Changes

--- alb.tf
+++ alb.tf
@@ -1,8 +1,15 @@
 resource "aws_lb_target_group" "tg" {
-  name = "${var.COMPONENT}-${var.ENV}"
-  port = var.PORT
-  protocol = "HTTP"
-  vpc_id = data.terraform_remote_state.vpc.outputs.VPC_ID
+  name      = "${var.COMPONENT}-${var.ENV}"
+  port      = var.PORT
+  protocol  = "HTTP"
+  vpc_id    = data.terraform_remote_state.vpc.outputs.VPC_ID
+  health_check {
+    enabled             = true
+    path                = "/health"
+    unhealthy_threshold = 2
+    interval            = 5
+    timeout             = 4
+  }
 }
 
 resource "aws_lb_target_group_attachment" "tg-attach" {
