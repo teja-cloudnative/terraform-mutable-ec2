5d27ce821326c9c3a59d6da2e12d688efa396669
tejap <btejapramod@gmail.com>
2022-03-04 12:10:48 +0530

    New Changes

--- alb.tf
+++ alb.tf
@@ -35,19 +35,18 @@ resource "random_integer" "priority" {
   max = 500
   keepers = {
     # Generate a new integer each time we switch to a new listener ARN
-    listener_arn = data.terraform_remote_state.alb.backend-listener-arn
+    listener_arn = data.terraform_remote_state.alb.backend_listener_arn
   }
 }
 
 resource "aws_lb_listener_rule" "static" {
-  count              = var.ALB_ATTACH_TO == "backend" ? 1 : 0
-  listener_arn = data.terraform_remote_state.alb.backend-listener-arn
-  priority     = aws_lb_listener.ba
+  count        = var.ALB_ATTACH_TO == "backend" ? 1 : 0
+  listener_arn = data.terraform_remote_state.alb.backend_listener_arn
+  priority     = random_integer.priority.result
 
   action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.tg.arn
-    prio
   }
 
   condition {
@@ -58,7 +57,7 @@ resource "aws_lb_listener_rule" "static" {
 
   condition {
     host_header {
-      values = ["example.com"]
+      values = ["${var.COMPONENT}-${var.ENV}.roboshop.internal"]
     }
   }
 }
