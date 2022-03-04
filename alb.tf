ee23a075d7181c5f6bcdf0a84f414fa16f7081ad
tejap <btejapramod@gmail.com>
2022-03-04 11:25:01 +0530

    New Changes

--- alb.tf
+++ alb.tf
@@ -30,13 +30,13 @@ resource "aws_lb_listener" "frontend" {
   }
 }
 
-#resource "aws_lb_listener" "backend"{
-#  count              = var.ALB_ATTACH_TO == "backend" ? 1 : 0
-#  load_balancer_arn  = data.terraform_remote_state.alb.outputs.frontend-arn
-#  port               = "8080"
-#  protocol           = "HTTP"
-#  default_action {
-#    type             = "forward"
-#    target_group_arn = aws_lb_target_group.tg.arn
-#  }
-#}
+resource "aws_lb_listener" "backend"{
+  count              = var.ALB_ATTACH_TO == "backend" ? 1 : 0
+  load_balancer_arn  = data.terraform_remote_state.alb.outputs.frontend-arn
+  port               = "8080"
+  protocol           = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
