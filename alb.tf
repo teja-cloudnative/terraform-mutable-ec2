7419fe1da8464c02dd5b65370401a1e410ecd892
tejap <btejapramod@gmail.com>
2022-03-04 12:14:32 +0530

    New Changes

--- alb.tf
+++ alb.tf
@@ -35,13 +35,13 @@ resource "random_integer" "priority" {
   max = 500
   keepers = {
     # Generate a new integer each time we switch to a new listener ARN
-    listener_arn = data.terraform_remote_state.alb.backend_listener_arn
+    listener_arn = data.terraform_remote_state.alb.outputs.backend_listener_arn
   }
 }
 
 resource "aws_lb_listener_rule" "static" {
   count        = var.ALB_ATTACH_TO == "backend" ? 1 : 0
-  listener_arn = data.terraform_remote_state.alb.backend_listener_arn
+  listener_arn = data.terraform_remote_state.alb.outputs.backend_listener_arn
   priority     = random_integer.priority.result
 
   action {
